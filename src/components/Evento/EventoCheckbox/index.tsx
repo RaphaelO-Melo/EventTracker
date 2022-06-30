03eb6099c0d03e915c1af05709e81cbf6f348bb5
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-06-30 19:57:56 -0300

    Using useSetRecoilState to set event as completed

--- src/components/Evento/EventoCheckbox/index.tsx
+++ src/components/Evento/EventoCheckbox/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
 import { IEvento } from '../../../interfaces/IEvento';
+import { listaDeEventosState } from '../../../state/atom';
 
-const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) => {
+const EventoCheckbox: React.FC<{ evento: IEvento}> = ({ evento }) => {
   
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+
+  const alterarStatus = () => {
+
+    const novoEvt = {...evento}
+    novoEvt.completo = !novoEvt.completo;
+
+    setListaDeEventos(listaAntiga => {
+      const index = listaAntiga.findIndex( evt => evt.id === evt.id);
+      return [...listaAntiga.slice(0, index), novoEvt, ...listaAntiga.slice(index + 1)];
+    });
+  }
+
   const estilos = [
     'far',
     'fa-2x',
     evento.completo ? 'fa-check-square' : 'fa-square'
   ]
 
-  return (<i className={estilos.join(' ')} onClick={() => aoAlterarStatus(evento.id!)}></i>)
+  return (<i className={estilos.join(' ')} onClick={alterarStatus}></i>)
 }
 
 export default EventoCheckbox
